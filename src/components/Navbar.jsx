31753682f676cdfed52f861823211386895cffb2
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-03-29 10:50:41 +0300

    Refactor Navbar component to include cart count and improve layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useCart } from '../context/CartContext'
 
 const Navbar = () => {
+  const { getCartCount } = useCart()
+  const cartCount = getCartCount()
+
   return (
-    <nav>
-      <h1 className='text-red-500'>Fruit Store</h1>
-      <ul>
-        <li>
-          <Link to='/'>Fruits</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-inner">
+        <Link to="/" className="brand-link">
+          <div className="brand-dot"></div>
+          <h1 className='text-red-500'>Fruit Store</h1>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to='/'>Fruits</Link>
+          </li>
+          <li>
+            <Link to='/cart'>
+              Cart {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+            </Link>
+          </li>
+          <li>
+            <Link to='/admin'>Admin</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
